d7f1ea02a5d4c69bc39cd389cce8d62e30ef8916
glyaxz <84515834+glyaxz@users.noreply.github.com>
2022-09-27 17:29:56 +0200

    add repository

---
+++ Proyectos/src/ejemplosT1/Ejemplo3.java
@@ -0,0 +1,16 @@
+package ejemplosT1;
+import java.io.*;
+
+
+public class Ejemplo3 {
+	public static void main(String[] args) {
+		String directory = args[0];
+		File f = new File(directory);
+		String[] list = f.list();
+		System.out.println("Ficheros dentro del directorio "+directory+": "+list.length);
+		for(int i = 0; i < list.length; i++) {
+			File f2 = new File(f, list[i]);
+			System.out.printf("Nombre: %s, ¿Es fichero?: %b, ¿Es directorio?: %b %n", list[i], f2.isFile(), f2.isDirectory());
+		}
+	}
+}
---
+++ Proyectos/src/ejerciciosT1/Ejercicio2.java
@@ -0,0 +1,10 @@
+package ejerciciosT1;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
